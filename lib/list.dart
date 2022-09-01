a34ebfe3f2028c804999527a1e49a4cc542d41e0
Kingsley Boahen <kingsleyboahen34@gmail.com>
2022-09-01 11:23:42 -0800

    first commit

--- lib/list.dart
+++ lib/list.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_app/breaking_bad_api.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -5,43 +6,45 @@ import 'package:flutter/src/widgets/framework.dart';
 class MyListApp extends StatelessWidget {
   MyListApp({Key? key}) : super(key: key);
 
-  List Users = [
-    {
-      "name": "Junior",
-      "age": 18,
-    },
+  // List Users = [
+  //   {
+  //     "name": "Junior",
+  //     "age": 18,
+  //   },
 
-    {
-      "name": "Francis",
-      "age": 20,
-    },
+  //   {
+  //     "name": "Francis",
+  //     "age": 20,
+  //   },
 
-    {
-      "name": "James",
-      "age": 22,
-    },
+  //   {
+  //     "name": "James",
+  //     "age": 22,
+  //   },
 
-    {
-      "name": "Baah",
-      "age": 23,
-    },
+  //   {
+  //     "name": "Baah",
+  //     "age": 23,
+  //   },
 
-    {
-      "name": "Selina",
-      "age": 19,
-    },
-  ];
+  //   {
+  //     "name": "Selina",
+  //     "age": 19,
+  //   },
+  // ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.builder(
-        itemCount: 5,
+        itemCount: breakingBadApi.length,
         itemBuilder: (context, index) {
          return ListTile(
-          leading: CircleAvatar(),
-          title: Text(Users[index]["name"]),
-          subtitle: Text("Good evening"),
+          leading: CircleAvatar(
+            backgroundImage: NetworkImage(breakingBadApi[index]["img"]),
+          ),
+          title: Text(breakingBadApi[index]["name"]),
+          subtitle: Text(breakingBadApi[index]["nickname"]),
           trailing: Column(
             children: [
               Text("10.00"),
